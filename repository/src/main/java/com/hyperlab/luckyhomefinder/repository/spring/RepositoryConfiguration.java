97b30b1b2dd6a25b1b2fa0b312470f6f1ded554e
Kareem El-Shahawe <kareemelshahawe@v-cumulus.com>
2014-01-15 21:06:28 +0800

    Refactored project modules and removed projects that are not needed.

--- repository/src/main/java/com/hyperlab/luckyhomefinder/repository/spring/RepositoryConfiguration.java
+++ repository/src/main/java/com/hyperlab/luckyhomefinder/repository/spring/RepositoryConfiguration.java
@@ -11,45 +11,48 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+
 import com.mongodb.Mongo;
 
 @Configuration
 @PropertySource("classpath:mongodb.properties")
-@ComponentScan(basePackages = { "com.luckyhomefinder.repository" })
-@EnableMongoRepositories("com.luckyhomefinder.repository")
+@ComponentScan(basePackages = { "com.hyperlab.luckyhomefinder.repository" })
+@EnableMongoRepositories("com.hyperlab.luckyhomefinder.repository")
 public class RepositoryConfiguration extends AbstractMongoConfiguration {
-	@Value("${mongodb.username}")
-	private String username;
-	@Value("${mongodb.password}")
-	private String password;
-	@Value("${mongodb.database}")
-	private String database;
-	@Value("${mongodb.url}")
-	private String url;
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		org.springframework.context.support.PropertySourcesPlaceholderConfigurer properties = new PropertySourcesPlaceholderConfigurer();
-		Resource[] resources = new ClassPathResource[] { new ClassPathResource(
-				"mongodb.properties") };
-		properties.setLocations(resources);
-		properties.setIgnoreResourceNotFound(true);
-		return properties;
-	}
+    @Value("${mongodb.username}")
+    private String username;
+    @Value("${mongodb.password}")
+    private String password;
+    @Value("${mongodb.database}")
+    private String database;
+    @Value("${mongodb.url}")
+    private String url;
 
-	@Bean
-	public MongoTemplate mongoTemplate() throws Exception {
-		return new MongoTemplate(mongoDbFactory(), mappingMongoConverter());
-	}
 
-	@Override
-	protected String getDatabaseName() {
-		return database;
-	}
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        org.springframework.context.support.PropertySourcesPlaceholderConfigurer properties = new PropertySourcesPlaceholderConfigurer();
+        Resource[] resources = new ClassPathResource[] { new ClassPathResource(
+                "mongodb.properties") };
+        properties.setLocations(resources);
+        properties.setIgnoreResourceNotFound(true);
+        return properties;
+    }
 
-	@Override
-	public Mongo mongo() throws Exception {
-		return new Mongo(url);
-	}
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoDbFactory(), mappingMongoConverter());
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new Mongo(url);
+    }
 
 }
--- repository/src/test/java/com/hyperlap/luckyhomefinder/repositry/test/propertyRepositoryTest.java
+++ repository/src/test/java/com/hyperlap/luckyhomefinder/repositry/test/propertyRepositoryTest.java
@@ -5,8 +5,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,54 +17,57 @@ import com.hyperlab.luckyhomefinder.common.domain.Property;
 import com.hyperlab.luckyhomefinder.common.domain.PropertyType;
 import com.hyperlab.luckyhomefinder.repository.PropertyRepository;
 import com.hyperlab.luckyhomefinder.repository.spring.RepositoryConfiguration;
+
 import static org.junit.Assert.assertNotNull;
 
 public class propertyRepositoryTest {
-	PropertyRepository propertyRepository;
 
-	@Before
-	public void setup() {
-		ApplicationContext context = new AnnotationConfigApplicationContext(
-				RepositoryConfiguration.class);
-		propertyRepository = context.getBean(PropertyRepository.class);
-	}
+    PropertyRepository propertyRepository;
 
-	@Test
-	public void testRepositoryOperations() throws Exception {
-		propertyRepository.deleteAll();
-		List<Property> properties = initProperties();
-		propertyRepository.save(properties);
-		assertNotNull(properties);
-	}
+    @Before
+    public void setup() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+                RepositoryConfiguration.class);
+        propertyRepository = context.getBean(PropertyRepository.class);
+    }
 
-	private List<Property> initProperties() throws Exception {
-		List<Property> properties = new ArrayList<Property>();
-		DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd aa.hh:mm");
+    @Test
+    public void testRepositoryOperations() throws Exception {
+        propertyRepository.deleteAll();
+        List<Property> properties = initProperties();
+        List<Property> savedProperties = propertyRepository.save(properties);
+        assertNotNull(properties);
+        assertNotNull(savedProperties);
+    }
 
-		properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
-				dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
-				PropertyType.APARTMENT, true, false,
-				"http://eng.28hse.com/utf8/property106671.html", true, true));
-		properties.add(new Property(UUID.randomUUID(), "10021335", "SHATIN",
-				dateFormat.parse("1925.05.10 PM.12:33"), 12535, 520000000l,
-				200, PropertyType.APARTMENT, true, false,
-				"http://eng.28hse.com/utf8/property10226671.html", true, true));
-		properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
-				dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
-				PropertyType.APARTMENT, true, false,
-				"http://eng.28hse.com/utf8/property106671.html", true, true));
-		properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
-				dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
-				PropertyType.APARTMENT, true, false,
-				"http://eng.28hse.com/utf8/property106671.html", true, true));
-		properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
-				dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
-				PropertyType.APARTMENT, true, false,
-				"http://eng.28hse.com/utf8/property106671.html", true, true));
-		properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
-				dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
-				PropertyType.APARTMENT, true, false,
-				"http://eng.28hse.com/utf8/property106671.html", true, true));
-		return properties;
-	}
+    private List<Property> initProperties() throws Exception {
+        List<Property> properties = new ArrayList<Property>();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd aa.hh:mm");
+
+        properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
+                dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
+                PropertyType.APARTMENT, true, false,
+                "http://eng.28hse.com/utf8/property106671.html", true, true));
+        properties.add(new Property(UUID.randomUUID(), "10021335", "SHATIN",
+                dateFormat.parse("1925.05.10 PM.12:33"), 12535, 520000000l,
+                200, PropertyType.APARTMENT, true, false,
+                "http://eng.28hse.com/utf8/property10226671.html", true, true));
+        properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
+                dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
+                PropertyType.APARTMENT, true, false,
+                "http://eng.28hse.com/utf8/property106671.html", true, true));
+        properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
+                dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
+                PropertyType.APARTMENT, true, false,
+                "http://eng.28hse.com/utf8/property106671.html", true, true));
+        properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
+                dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
+                PropertyType.APARTMENT, true, false,
+                "http://eng.28hse.com/utf8/property106671.html", true, true));
+        properties.add(new Property(UUID.randomUUID(), "1002435", "SHATIN",
+                dateFormat.parse("1925.05.10 PM.12:33"), 1235, 50000000l, 200,
+                PropertyType.APARTMENT, true, false,
+                "http://eng.28hse.com/utf8/property106671.html", true, true));
+        return properties;
+    }
 }
