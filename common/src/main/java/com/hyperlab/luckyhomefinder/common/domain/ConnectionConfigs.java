d387dead32d51dcff2bf7a2f66ec625e871bf1f5
jason <jason@jason.(none)>
2014-03-11 17:40:36 +0800

    -Added documentation for generic config class

--- common/src/main/java/com/hyperlab/luckyhomefinder/common/domain/ConnectionConfigs.java
+++ common/src/main/java/com/hyperlab/luckyhomefinder/common/domain/ConnectionConfigs.java
@@ -1,10 +1,12 @@
 package com.hyperlab.luckyhomefinder.common.domain;
 
-import org.springframework.stereotype.Component;
-
-@Component
+/**
+ * Enum holding generic connection configuration used to initiate connection to
+ * any of the target website.
+ * */
 public enum ConnectionConfigs {
-	USERAGENT("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0");
+	USERAGENT(
+			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0");
 
 	private String param;
 
